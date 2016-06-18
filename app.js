5e8f279976316e34e7b0f23abd1626c167fab9b4
Blake Guilloud <fbguillo@gmail.com>
2016-06-17 20:45:13 -0400

    changing port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const Todo = require('./Todo.model');
 
 const db = 'mongodb://localhost/test12';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 mongoose.connect(db);
 
